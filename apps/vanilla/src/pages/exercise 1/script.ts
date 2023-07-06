a9818605be1363c7db41d57a816af05d0c7d3938
Irina Tumanova <irina.tumanova@saritasa.com>
2023-07-06 11:19:05 +0500

    Правки линтера

--- apps/vanilla/src/pages/exercise 1/script.ts
+++ apps/vanilla/src/pages/exercise 1/script.ts
@@ -1,42 +1,53 @@
+/** Header height. */
 const headerHeight = 72;
+
+/** Filters component. */
 const filtersComponent = document.querySelector('.filters');
 
-function fixFilters(){
-	if(filtersComponent === null) return;
+/** Fix filters on top. */
+function fixFilters(): void {
+	if (filtersComponent === null) {
+		return;
+	}
 	addEventListener('scroll', () => {
-		if(window.scrollY >= headerHeight){
+		if (window.scrollY >= headerHeight) {
 			filtersComponent.classList.add('fixed');
 		} else {
-			filtersComponent.classList.remove('fixed')
+			filtersComponent.classList.remove('fixed');
 		}
-	})
+	});
 }
 
-function fixBookCategory(){
+/** Fix book categories on top. */
+function fixBookCategory(): void {
 	const bookLists = document.querySelectorAll('.book-list');
-	const maxWidthWindowWithOutsideFilters = 1000
+	const maxWidthWindowWithOutsideFilters = 1000;
 
 	addEventListener('scroll', () => {
-		const scrollY = window.scrollY;
-		let offset = window.innerWidth <= maxWidthWindowWithOutsideFilters ?
+		const { scrollY } = window;
+		const offset = window.innerWidth <= maxWidthWindowWithOutsideFilters ?
 			filtersComponent?.clientHeight ?? 0 :
-			0
+			0;
 
-		for(let i = 0; i < bookLists.length; i++){
+		for (let i = 0; i < bookLists.length; i++) {
 			const bookList = bookLists[i];
 			const position = bookList.getBoundingClientRect();
 
 			const category = bookList.querySelector('.book-category');
-			if(category === null) continue;
+			if (category === null) {
+				continue;
+			}
 
-			if(position.top <= offset && scrollY >= headerHeight) category.classList.add('fixed')
-			else category.classList.remove('fixed')
+			if (position.top <= offset && scrollY >= headerHeight) {
+				category.classList.add('fixed');
+			} else {
+				category.classList.remove('fixed');
+			}
 
 			category.setAttribute('style', `top: ${offset}px`);
 		}
-	})
+	});
 }
 
-
-fixBookCategory()
-fixFilters()
+fixBookCategory();
+fixFilters();
--- apps/vanilla/vite.config.ts
+++ apps/vanilla/vite.config.ts
@@ -1,29 +1,30 @@
-import { resolve } from 'path';
+import {resolve} from 'path';
 
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
 const root = resolve(__dirname, 'src/pages');
 const outDir = resolve(__dirname, '../../dist/apps/vanilla');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
-  plugins: [],
-  resolve: {
-    alias: {
-      '@js-camp/vanilla': resolve(__dirname, 'src'),
-      '@js-camp': resolve(__dirname, '../../libs'),
-    },
-  },
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: [
-        resolve(root, 'index.html'),
-        resolve(root, 'example', 'index.html'),
-        resolve(root, 'example', 'nested', 'index.html'),
-      ],
-    },
-  },
+	root,
+	plugins: [],
+	resolve: {
+		alias: {
+			'@js-camp/vanilla': resolve(__dirname, 'src'),
+			'@js-camp': resolve(__dirname, '../../libs'),
+		},
+	},
+	build: {
+		outDir,
+		emptyOutDir: true,
+		rollupOptions: {
+			input: [
+				resolve(root, 'index.html'),
+				resolve(root, 'example', 'index.html'),
+				resolve(root, 'exercise 1', 'index.html'),
+				resolve(root, 'example', 'nested', 'index.html'),
+			],
+		},
+	},
 });
