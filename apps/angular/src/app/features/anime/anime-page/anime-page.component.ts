1d18e8311256075af5fbeea6b74a6a4bf70428f5
irinatumanova <irina.tumanova@saritasa.com>
2023-07-20 15:23:00 +0500

    Add filters oin params(doesn,t work) JC-544

--- apps/angular/src/app/features/anime/anime-page/anime-page.component.ts
+++ apps/angular/src/app/features/anime/anime-page/anime-page.component.ts
@@ -1,14 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { PageEvent } from '@angular/material/paginator';
-import { Sort } from '@angular/material/sort';
+import { Sort, SortDirection } from '@angular/material/sort';
 import { AnimeService } from '@js-camp/angular/core/services/anime.service';
 import { Anime, AnimeType } from '@js-camp/core/models/anime';
 import { AnimeSortingField, AnimeParams } from '@js-camp/core/models/anime-params';
 import { AnimeStatus } from '@js-camp/core/models/anime-status';
 import { Pagination } from '@js-camp/core/models/pagination';
-import { BehaviorSubject, Observable, tap, map, debounceTime, switchMap, shareReplay, combineLatestWith, startWith } from 'rxjs';
+import { BehaviorSubject, Observable, tap, map, debounceTime, switchMap, shareReplay, combineLatestWith, startWith, merge, Subscription, take } from 'rxjs';
 import { Sorting } from '@js-camp/core/models/sorting';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 const defaultParams: AnimeParams = {
 	limit: 10,
@@ -29,7 +30,7 @@ const REQUEST_DEBOUNCE_TIME = 500;
 	styleUrls: ['./anime-page.component.css'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AnimePageComponent {
+export class AnimePageComponent implements OnDestroy, OnInit {
 
 	/** Columns in a table. */
 	protected readonly displayedColumns: readonly string[] = ['image', 'titleEnglish', 'titleJapanese', 'aired.start', 'type', 'status'];
@@ -63,7 +64,15 @@ export class AnimePageComponent {
 	/** Number of elements per page. */
 	protected limit = defaultParams.limit;
 
+	private readonly params$: Observable<AnimeParams>;
+
+	private sideEffectsSubscription?: Subscription;
+
 	private readonly animeService = inject(AnimeService);
+
+	private readonly route = inject(ActivatedRoute);
+
+	private readonly router = inject(Router);
 
 	/** Filters form: search and type filter. */
 	protected readonly filtersForm = new FormGroup({
@@ -71,19 +80,71 @@ export class AnimePageComponent {
 		type: new FormControl(defaultParams.filters.type),
 	});
 
-	/**
-	 * @param animeService Anime request service.
-	 */
 	public constructor() {
-		this.animeList$ = this.createAnimeListStream();
+		this.params$ = this.createParamsStream();
+
+		this.animeList$ = this.params$.pipe(
+			tap(() => this.isLoading$.next(true)),
+			switchMap(params => this.animeService.getAnime(params)),
+			tap(() => this.isLoading$.next(false)),
+			shareReplay({ refCount: true, bufferSize: 1 }),
+		);
+	}
+
+	/** @inheritdoc */
+	public ngOnInit(): void {
+		this.sideEffectsSubscription = this.createSideEffectsSubscription();
+	}
+
+	/** @inheritdoc */
+	public ngOnDestroy(): void {
+		this.sideEffectsSubscription?.unsubscribe();
+	}
+
+	private createSideEffectsSubscription(): Subscription {
+		const setFiltersFromParams$ = this.route.queryParams.pipe(
+			tap(params => this.setFiltersFromParams(params)),
+			take(1),
+		);
+
+		const resetPagination$ = this.filtersForm.valueChanges.pipe(
+			tap(() => this.page$.next(0)),
+		);
+
+		const scrollToTopAfterChangePage$ = this.page$.pipe(
+			tap(() => this.scrollToTopPage()),
+		);
+
+		const navigateByFilters$ = this.params$.pipe(
+			tap(({ limit, page, sorting, filters }) => {
+				const queryParams = {
+					limit,
+					page,
+					field: sorting.field,
+					direction: sorting.direction,
+					search: filters.search,
+					type: filters.type,
+				};
+				this.router.navigate([], {
+					queryParams,
+					queryParamsHandling: 'merge',
+				});
+			}),
+		);
+
+		return merge(
+			setFiltersFromParams$,
+			resetPagination$,
+			scrollToTopAfterChangePage$,
+			navigateByFilters$,
+		).subscribe();
 	}
 
 	/** Create anime list stream. */
-	private createAnimeListStream(): Observable<Pagination<Anime>> {
+	private createParamsStream(): Observable<AnimeParams> {
 		return this.page$.pipe(
 			combineLatestWith(
 				this.filtersForm.valueChanges.pipe(
-					tap(() => this.page$.next(0)),
 					startWith(this.filtersForm.value),
 				),
 				this.sorting$,
@@ -102,11 +163,28 @@ export class AnimePageComponent {
 
 				return params;
 			}),
-			tap(() => this.isLoading$.next(true)),
-			switchMap(params => this.animeService.getAnime(params)),
-			tap(() => this.isLoading$.next(false)),
-			shareReplay({ refCount: true, bufferSize: 1 }),
 		);
+	}
+
+	/**
+	 * Set filters from params.
+	 * @param params Params: sorting + type + search.
+	 */
+	private setFiltersFromParams(params: Params): void {
+		if (params['type']) {
+			this.filtersForm.controls.type.setValue(params['type'] as AnimeType[]);
+		}
+
+		if (params['search']) {
+			this.filtersForm.controls.search.setValue(params['search']);
+		}
+
+		this.sorting$.next({
+			field: params['field'] as AnimeSortingField ?? defaultParams.sorting.field,
+			direction: params['derection'] as SortDirection ?? defaultParams.sorting.direction,
+		});
+
+		this.page$.next(+(params['page'] ?? defaultParams.page));
 	}
 
 	/**
@@ -141,5 +219,14 @@ export class AnimePageComponent {
 	 */
 	protected trackType(index: number, type: AnimeType): AnimeType {
 		return type;
+	}
+
+	/** Scroll to top. */
+	private scrollToTopPage(): void {
+		window.scroll({
+			top: 0,
+			left: 0,
+			behavior: 'smooth',
+		});
 	}
 }
