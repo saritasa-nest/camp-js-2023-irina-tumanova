9653938938b86a0df0ca85e05ab9a7725fd2bcb3
irinatumanova <irina.tumanova@saritasa.com>
2023-07-31 14:14:24 +0500

    Fix password validator (add passwordName params) JC-551

--- apps/angular/src/core/utils/validators.ts
+++ apps/angular/src/core/utils/validators.ts
@@ -5,11 +5,21 @@ export namespace AppValidators{
 
 	export const MIN_PASSWORD_LENGTH = 8;
 
-	/** Check if the entered passwords match. */
-	export function passwordRepetition(): ValidatorFn {
+	/**
+	 * Check if the entered passwords match.
+	 * @param originalPasswordName Control name of original password.
+	 *  @param repeatedPasswordName Control name of repeated password.
+	 */
+	export function passwordRepetition(originalPasswordName: string, repeatedPasswordName: string): ValidatorFn {
 		return (group: AbstractControl): ValidationErrors | null => {
 			if (group instanceof FormGroup) {
-				const { password, repeatedPassword } = group.controls;
+				const password = group.controls[originalPasswordName];
+				const repeatedPassword = group.controls[repeatedPasswordName];
+
+				if (password === undefined || repeatedPassword === undefined) {
+					return null;
+				}
+
 				let repeatedPasswordErrors = repeatedPassword.errors;
 
 				if (password.value !== repeatedPassword.value) {
