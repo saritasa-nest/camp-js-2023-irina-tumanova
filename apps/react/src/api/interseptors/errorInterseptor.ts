e5b05ed4af06b7a9ae65ffb3e6eb8a28bfe9cefe
irinatumanova <irina.tumanova@saritasa.com>
2023-08-11 17:49:12 +0500

    Delete errorInterseptor JC-572

--- apps/react/src/api/interseptors/errorInterseptor.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosError, AxiosResponse } from 'axios';
-
-/**
- * Intercept error .
- * @param error Axios error object.
- */
-export function errorInterceptor(error: AxiosError): Promise<AxiosResponse<unknown, unknown>> {
-	throw error;
-}
