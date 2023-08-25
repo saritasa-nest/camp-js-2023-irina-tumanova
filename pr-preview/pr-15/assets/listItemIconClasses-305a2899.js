bee6009726f3833a2e8b611f1c59f04f604a9a16
Emil-Arzumanov <Emil-Arzumanov@users.noreply.github.com>
2023-08-25 13:42:18 +0000

    Deploy preview for PR 15 🛫

--- pr-preview/pr-15/assets/listItemIconClasses-305a2899.js
+++
@@ -1,1 +0,0 @@
-import{t as s,v as e}from"./index-4fb02bcc.js";function l(t){return e("MuiListItemIcon",t)}const i=s("MuiListItemIcon",["root","alignItemsFlexStart"]),n=i;export{l as g,n as l};
---
+++ pr-preview/pr-15/assets/listItemIconClasses-31f47e27.js
@@ -0,0 +1,1 @@
+import{t as s,v as e}from"./index-e8cd6ef5.js";function l(t){return e("MuiListItemIcon",t)}const i=s("MuiListItemIcon",["root","alignItemsFlexStart"]),n=i;export{l as g,n as l};
